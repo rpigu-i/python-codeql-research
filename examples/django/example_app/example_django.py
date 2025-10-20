7219fc39eeb2f79be29a6dc40fbfa1482e22f1fb
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2025-10-20 16:16:31 -0400

    Add in SQLi

--- examples/django/example_app/example_django.py
+++
@@ -1,5 +0,0 @@
-from django.db import connection
-
-def show_user(request, username):
-   with connection.cursor() as cursor:
-       cursor.execute("SELECT * FROM users WHERE username = '%s'" % username)
--- examples/django/example_app/views.py
+++ examples/django/example_app/views.py
@@ -1,3 +1,4 @@
+from django.db import connection
 from django.shortcuts import render
 
 def my_view(request):
@@ -6,3 +7,8 @@ def my_view(request):
     # Attempting to access a non-existent key will cause an error
     print(data["age"]) 
     return render(request, 'myapp/index.html', {'data': data})
+
+
+def show_user(request, username):
+   with connection.cursor() as cursor:
+       cursor.execute("SELECT * FROM users WHERE username = '%s'" % username)
